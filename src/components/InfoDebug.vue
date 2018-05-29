56ef500a0044fb735d6965f6d5b6871c3205714a
foticc <foticc@163.com>
2018-05-30 00:11:00 +0800

    全局CSS，cell文件居右

--- src/components/InfoDebug.vue
+++ src/components/InfoDebug.vue
@@ -1,14 +1,17 @@
 <template>
-  <div class="page-cell-wrapper">
-    <mt-cell title="标题文字" value="说明文字"></mt-cell>
-    <mt-cell title="标题文字" is-link>
-      <span style="color: green">这里是元素</span>
-    </mt-cell>
-
+  <div id="panel">
+    Lv{{level}}/40<mt-range v-model="level" :max=40 :min=1 borHeigh=20 ></mt-range>
+    <mt-cell title="攻击" value="说明文字"></mt-cell>
+    <mt-cell title="生命" ></mt-cell>
+    <mt-cell title="防御" value="说明文字"></mt-cell>
+    <mt-cell title="速度"></mt-cell>
+    <mt-cell title="暴击" value="说明文字"></mt-cell>
+ 
 </div>
 </template>
 
 <script>
+import { info } from "./info.vue";
 export default {
   name: 'InfoDebug',
   data (){
@@ -26,5 +29,7 @@ export default {
 
 
 <style>
-
+#panel{
+  font-family: 'Console'
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: InfoDebug
+      component: Home
     },
     {
       path: '/info/:id',
